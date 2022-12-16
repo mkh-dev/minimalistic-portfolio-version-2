b64ce037e1ed214d2f19aee77bbc75b6d8752437
Malek Khelil <112271600+mkh-dev@users.noreply.github.com>
2022-12-17 00:29:57 +0100

    Add files via upload

---
+++ scriptEN.js
@@ -0,0 +1,47 @@
+let darkMode = false;
+
+function changeDarkMode() {
+  if (darkMode) {
+    darkMode = false;
+    document.documentElement.style.setProperty("--text-color", "black");
+    document.documentElement.style.setProperty("--background-color", "#efe7e5");
+    document.getElementById("dark-light-mode").innerHTML = "Dark Mode";
+    document.getElementById("light-mode").innerHTML="dark_mode"
+  } else {
+    darkMode = true 
+    document.documentElement.style.setProperty("--text-color", "white");
+    document.documentElement.style.setProperty("--background-color", "black");
+    document.getElementById("dark-light-mode").innerHTML = "Light Mode";
+    document.getElementById("light-mode").innerHTML="light_mode"
+  }
+}
+
+
+const goTopBtn = document.querySelector('.go-top-btn');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 200) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
+}
+
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
+
+
+
+
+
+
+
+
+
+
